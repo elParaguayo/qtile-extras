0aefabef236178bef27a79c0a923cdda4125cc18
elParaguayo <elparaguayocode@gmail.com>
2021-11-27 12:18:06 +0000

    Move upower dbus to conftest

--- test/widget/test_upower.py
+++ test/widget/test_upower.py
@@ -17,28 +17,19 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-import asyncio
 import os
 import shutil
-import signal
 import subprocess
-import time
-from threading import Thread
 
 import libqtile.bar
 import libqtile.config
 import libqtile.confreader
 import libqtile.layout
 import pytest
-from dbus_next.aio import MessageBus
-from dbus_next.constants import BusType, PropertyAccess
-from dbus_next.service import ServiceInterface, dbus_property, method
+from dbus_next.constants import BusType
 
 from qtile_extras.widget.upower import UPowerWidget
 from test.helpers import Retry  # noqa: I001
-
-BAT0 = "/org/freedesktop/UPower/device/battery_BAT0"
-BAT1 = "/org/freedesktop/UPower/device/battery_BAT1"
 
 
 @Retry(ignore_exceptions=(AssertionError, ))
@@ -56,82 +47,6 @@ def battery_found(manager):
 def text_hidden(manager, target):
     """Waits for widget to hide text."""
     assert manager.c.widget["upowerwidget"].info()["width"] == target
-
-
-class Power(ServiceInterface):
-    def __init__(self, *args, **kwargs):
-        ServiceInterface.__init__(self, *args, **kwargs)
-        self._charging = False
-
-    @method()
-    def enumerate_devices(self) -> 'ao':  # noqa: F821
-        return [BAT0, BAT1]
-
-    @dbus_property(access=PropertyAccess.READ)
-    def on_battery(self) -> 'b':  # noqa: F821
-        return not self._charging
-
-    @method()
-    def toggle_charge(self):
-        self._charging = not self._charging
-        self.emit_properties_changed({'on_battery': self._charging})
-
-
-class Battery(ServiceInterface):
-    def __init__(self, native_name, server, *args, **kwargs):
-        ServiceInterface.__init__(self, *args, **kwargs)
-        self._level = 50.0
-        self._native_name = native_name
-        self._server = server
-
-    @dbus_property(access=PropertyAccess.READ)
-    def Percentage(self) -> 'd':  # noqa: F821, N802
-        return self._level
-
-    @dbus_property(access=PropertyAccess.READ)
-    def TimeToFull(self) -> 'x':  # noqa: F821, N802
-        if self._server._charging:
-            return 3780
-        return 0
-
-    @dbus_property(access=PropertyAccess.READ)
-    def TimeToEmpty(self) -> 'x':  # noqa: F821, N802
-        if not self._server._charging:
-            return 12200
-        return 0
-
-    @dbus_property(access=PropertyAccess.READ)
-    def NativeName(self) -> 's':  # noqa: F821, N802
-        return self._native_name
-
-
-class FakeUpower(Thread):
-    """Class that runs fake UPower interface in a thread."""
-    def __init__(self, *args, **kwargs):
-        Thread.__init__(self, *args, **kwargs)
-
-    async def start_server(self):
-        """Connects to the bus and publishes 3 interfaces."""
-        bus = await MessageBus().connect()
-
-        # Create UPower and 2 battery interfaces
-        po = Power("org.freedesktop.UPower")
-        bat0 = Battery("BAT0", po, "org.freedesktop.UPower.Device")
-        bat1 = Battery("BAT1", po, "org.freedesktop.UPower.Device")
-
-        # Export interfaces on the bus
-        bus.export("/org/freedesktop/UPower", po)
-        bus.export(BAT0, bat0)
-        bus.export(BAT1, bat1)
-
-        # Request the service name
-        await bus.request_name("test.qtileextras.upower")
-
-        await asyncio.get_event_loop().create_future()
-
-    def run(self):
-        loop = asyncio.new_event_loop()
-        loop.run_until_complete(self.start_server())
 
 
 @pytest.fixture
@@ -167,59 +82,6 @@ def powerconfig(request, powerwidget):
         ]
 
     yield PowerConfig
-
-
-@pytest.fixture()
-def dbus_thread(monkeypatch):
-    """
-    Start a thread which publishes a fake UPower interface on dbus.
-
-    For some reason, I can't use the "dbus" fixture so I have to recreate
-    it here.
-    """
-    # for Github CI/Ubuntu, dbus-launch is provided by "dbus-x11" package
-    launcher = shutil.which("dbus-launch")
-
-    # If dbus-launch can't be found then tests will fail so we
-    # need to skip
-    if launcher is None:
-        pytest.skip("dbus-launch must be installed")
-
-    # dbus-launch prints two lines which should be set as
-    # environmental variables
-    result = subprocess.run(launcher, capture_output=True)
-
-    pid = None
-    for line in result.stdout.decode().splitlines():
-
-        # dbus server addresses can have multiple "=" so
-        # we use partition to split by the first one onle
-        var, _, val = line.partition("=")
-
-        # Use monkeypatch to set these variables so they are
-        # removed at end of test.
-        monkeypatch.setitem(os.environ, var, val)
-
-        # We want the pid so we can kill the process when the
-        # test is finished
-        if var == "DBUS_SESSION_BUS_PID":
-            try:
-                pid = int(val)
-            except ValueError:
-                pass
-
-    t = FakeUpower()
-    t.daemon = True
-    t.start()
-
-    # Pause for the dbus interface to come up
-    time.sleep(1)
-
-    yield
-
-    # Stop the bus
-    if pid:
-        os.kill(pid, signal.SIGTERM)
 
 
 upower_dbus_servive = pytest.mark.usefixtures("dbus_thread")
