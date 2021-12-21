1cab0a54475327ba6d321200c25f37ea2c124f69
elParaguayo <elparaguayocode@gmail.com>
2021-12-21 16:37:07 +0000

    Add another wallpaper

--- qtile_extras/resources/wallpapers/__init__.py
+++ qtile_extras/resources/wallpapers/__init__.py
@@ -2,8 +2,9 @@ from pathlib import Path
 
 WALLPAPER_TILES = Path(__file__).parent / "qte_tiles.png"
 WALLPAPER_TRIANGLES = Path(__file__).parent / "qte_triangles.png"
+WALLPAPER_TRIANGLES_ROUNDED = Path(__file__).parent / "qte_triangles_rounded.png"
 
-__all__ = ["WALLPAPER_TILES", "WALLPAPER_TRIANGLES"]
+__all__ = ["WALLPAPER_TILES", "WALLPAPER_TRIANGLES", "WALLPAPER_TRIANGLES_ROUNDED"]
 
 
 def __dir__():
