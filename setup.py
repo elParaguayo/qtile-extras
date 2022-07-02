63a80b05cfcf4c0d25ba065c95a1179e61f71f71
elParaguayo <elparaguayocode@gmail.com>
2022-07-02 20:05:38 +0100

    Better packaging
    Need to make sure icons etc are packaged.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='qtile-extras',
     packages=find_packages(),
     include_package_data=True,
-    version='0.1.0',
+    version="0.0.1",
     description='Extra items for qtile that are unlikely to be maintained in the main repo.',
     author='elParaguayo',
     url='https://github.com/elparaguayo/qtile-extras',
