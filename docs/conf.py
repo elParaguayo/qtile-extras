7a04a3d8291b10af3f60a4dba88a932578bb2ebe
elParaguayo <elparaguayocode@gmail.com>
2024-01-01 09:23:32 +0000

    Add generated screenshots

--- docs/conf.py
+++ docs/conf.py
@@ -118,6 +118,7 @@ html_theme = "sphinx_rtd_theme"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+html_extra_path = ["_static/screenshots"]
 
 
 # A workaround for the responsive tables always having annoying scrollbars.
